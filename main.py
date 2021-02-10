78b9480846324bcb5411e23b270174f7403f0f04
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-02-10 15:46:39 +0500

    Step 1

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import sys
+
+import ijson
+import json
+
+FILE_PATH = 'countries.json'
+
+
+class Founder:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        with open(file_path, encoding='utf-8') as f:
+            self.objects = ijson.items(f, 'item.name.common')
+            print(sys.getsizeof(self.objects))
+            countries_name = (o for o in self.objects)
+            for item in countries_name:
+                print(item)
+
+            self.countries = json.load(f)
+            print(sys.getsizeof(self.countries))
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        pass
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    founder = Founder(FILE_PATH)
