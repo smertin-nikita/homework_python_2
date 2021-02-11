499f6976ea324b8fa10c78ed76d526d4a9837410
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-02-11 14:17:33 +0500

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import sys
 import time
 
@@ -8,19 +9,24 @@ FILE_PATH = 'countries.json'
 
 
 def founder(file_path):
+    """
+    Гениратор, который по каждой стране из файла countries.json ищет страницу из википедии
+    :param file_path:
+    """
     with open(file_path, encoding='utf-8') as f:
         objects = ijson.items(f, 'item.name.common')
-        print(f'gen objects size = {sys.getsizeof(objects)}')
         for item in objects:
             yield {item: f"https://en.wikipedia.org/wiki/{item.replace(' ', '_')}"}
 
 
 class Founder:
+    """
+    Класс итератора, который по каждой стране из файла countries.json ищет страницу из википедии
+    """
     def __init__(self, file_path):
         self.file_path = file_path
         with open(file_path, encoding='utf-8') as f:
             self.countries = iter(json.load(f))
-        print(f'iter jsonload size = {sys.getsizeof(self.countries)}')
 
     def __iter__(self):
         return self
@@ -30,23 +36,35 @@ class Founder:
         return {country: f"https://en.wikipedia.org/wiki/{country.replace(' ', '_')}"}
 
 
+def get_hash(file_path):
+    """
+    Генератор, который принимает путь к файлу. При каждой итерации возвращает md5 хеш каждой строки файла.
+    :param file_path:
+    """
+    with open(file_path, encoding='utf-8') as f:
+        for line in f:
+            yield hashlib.md5(line.encode('utf-8')).hexdigest()
+
+
+def write_to_file(iterable, file_path):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        for item in iterable:
+            f.write(f'{item}\n')
+
+
 if __name__ == '__main__':
     start_time = time.time()
-    founder_class = Founder(FILE_PATH)
-    with open('countries_info.txt', 'w', encoding='utf-8') as f:
-        # founder_class_time = time.time() - founder_class_time
-        for item in founder_class:
-            f.write(f'{item}\n')
+    write_to_file(Founder(FILE_PATH), 'countries_info.txt')
     founder_class_time = time.time() - start_time
 
     start_time = time.time()
-    founder_gen = founder(FILE_PATH)
-    with open('countries_info_2.txt', 'w', encoding='utf-8') as f:
-        for item in founder_gen:
-            f.write(f'{item}\n')
+    write_to_file(founder(FILE_PATH), 'countries_info_2.txt')
     founder_time = time.time() - start_time
 
-    print(f"class size = {sys.getsizeof(founder_class)}")
-    print(f"gen size = {sys.getsizeof(founder_gen)}")
+    # Замерил скорость выполнения
     print(f"class time = {founder_class_time}")
     print(f"gen time = {founder_time}")
+
+    # Вывести hash md5 строк
+    for hash in get_hash('countries_info.txt'):
+        print(hash)
